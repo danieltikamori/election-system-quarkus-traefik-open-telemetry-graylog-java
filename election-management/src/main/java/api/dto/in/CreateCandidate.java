82dc9d6d6aecd110858e23d40316aae1084e9f43
danieltikamori <tikamori.itiro@gmail.com>
2024-04-07 17:01:24 +0900

    Add API layer classes.

---
+++ election-management/src/main/java/api/dto/in/CreateCandidate.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024 Daniel I. Tikamori. All rights reserved.
+ */
+
+package api.dto.in;
+
+import domain.Candidate;
+
+import java.util.Optional;
+
+public record CreateCandidate(Optional<String> photo,
+                              String givenName,
+                              String familyName,
+                              String email,
+                              Optional<String> phone,
+                              Optional<String> jobTitle) {
+    public Candidate toDomain() {
+        return Candidate.create(photo(), givenName(), familyName(), email(), phone(), jobTitle());
+    }
+}
---
+++ election-management/src/main/java/api/dto/out/Candidate.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2024 Daniel I. Tikamori. All rights reserved.
+ */
+
+package api.dto.out;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.Optional;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public record Candidate(String id,
+                        Optional<String> photo,
+                        String fullName,
+                        String email,
+                        Optional<String> phone,
+                        Optional<String> jobTitle) {
+    public static Candidate fromDomain(domain.Candidate candidate) {
+        return new Candidate(candidate.id(),
+                candidate.photo(),
+                candidate.givenName() + " " + candidate.familyName(),
+                candidate.email(),
+                candidate.phone(),
+                candidate.jobTitle());
+    }
+}
